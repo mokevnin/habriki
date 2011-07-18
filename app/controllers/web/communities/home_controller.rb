98a4ec5f3d4d1edc01064bc6da941b8fe78a38f7
Mokevnin Kirill <mokevnin@gmail.com>
2011-07-18 10:35:21 +0400

    fixed communities home controller

--- app/controllers/web/communities/home_controller.rb
+++ app/controllers/web/communities/home_controller.rb
@@ -1,6 +1,5 @@
 class Web::Communities::HomeController < Web::Communities::ApplicationController
   def index
-    CommunityMemberMailer.confirmation_instructions(community.members.last).deliver
     @posts = community.posts
   end
 end
