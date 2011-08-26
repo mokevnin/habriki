59f751bf9a9831e18a0e8738a5eba2d77a15504c
Mokevnin Kirill <mokevnin@gmail.com>
2011-08-26 17:13:25 +0400

    fixed community member mailer

--- app/mailers/community_member_mailer.rb
+++ app/mailers/community_member_mailer.rb
@@ -1,4 +1,7 @@
 class CommunityMemberMailer < ActionMailer::Base
+default :from => "habriki.com <info@habriki.com>",
+          'Content-Transfer-Encoding' => '7bit'
+
   def confirmation_instructions(community, member)
     @community = community
     @member = member
