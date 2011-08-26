1db0d89563510b4876b55d885df643f7ee14c005
Mokevnin Kirill <mokevnin@gmail.com>
2011-08-26 16:34:36 +0400

    fixed subdomain rewriter

--- lib/middlewares/rewrite_subdomain_to_path.rb
+++ lib/middlewares/rewrite_subdomain_to_path.rb
@@ -7,13 +7,14 @@ module Middlewares
     def call(env)
       server_name = env['SERVER_NAME']
       if server_name != '_'
-        http_host = env['HTTP_HOST'].gsub(/:[\d]+/, '')
+        http_host = (env['HTTP_HOST'] || '').gsub(/:[\d]+/, '')
         subdomain = http_host.gsub(/\.?#{server_name}/, '')
-          if subdomain.present? && subdomain !~ /www|mail/
-            env['PATH_INFO'] = ['communities', subdomain, env['PATH_INFO']].join('/')
-        env['HTTP_X_FORWARDED_HOST'] = [server_name, http_host].join(', ')
-        Rails.logger.info("Reroute: mapped #{http_host} => #{server_name}")
-          end
+        if subdomain.present? && subdomain !~ /www|mail/
+          env['PATH_INFO'] = ['communities', subdomain, env['PATH_INFO']].join('/')
+          env['HTTP_X_FORWARDED_HOST'] = [server_name, http_host].join(', ')
+          Rails.logger.info("Reroute: mapped #{http_host} => #{server_name}")
+          Rails.logger.info("Subdomain: #{subdomain}")
+        end
       end
 
       @app.call(env)
